6d08aa47e328cd75b44374c7289d1aae229247f9
zappybird <cg0597@gmail.com>
2026-03-04 22:01:20 -0500

    temp bruteforce attack function

--- Hash-crack/attacks/bruteforce.py
+++ Hash-crack/attacks/bruteforce.py
@@ -2,6 +2,12 @@
 
 def bruteforce_attack(target_hash: str, algorithm: str, charset: str, max_length: int, salt: str = ""):
     # TODO: generate combinations using itertools.product
-    # TODO: hash + compare
-    # TODO: stop when match found
-    pass
+    
+    for length in range(1, max_length + 1):
+        for combination in itertools.product(charset, repeat=length):
+            guess = "".join(combination)
+            if salt:
+                guess = salt + guess
+            if hash_function(guess, algorithm) == target_hash:
+                return guess
+    return None
