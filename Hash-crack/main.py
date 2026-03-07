45a7b450ff4802cfe4997aeb0972da7ca49ea23d
zappybird <cg0597@gmail.com>
2026-03-07 18:41:10 -0500

    Imported argparse lib and added arguments to parser

--- Hash-crack/main.py
+++ Hash-crack/main.py
@@ -1,3 +1,4 @@
+import argparse
 
 
 def parse_arguments():
@@ -9,6 +10,15 @@ def parse_arguments():
     # --charset
     # --max-length
     # --salt
+
+    parser = argparse.ArgumentParser(description="Hash cracking tool")
+    parser.add_argument("--hash", required=True, help="The hash to crack")
+    parser.add_argument("--algorithm", required=True, choices=["md5", "sha1", "sha256"], help="Hashing algorithm")
+    parser.add_argument("--mode", required=True, choices=["dictionary", "bruteforce"], help="Attack mode")
+    parser.add_argument("--wordlist", help="Path to the wordlist for dictionary attack")
+    parser.add_argument("--charset", help="Character set for brute-force attack")
+    parser.add_argument("--max-length", type=int, help="Maximum password length for brute-force attack")
+    parser.add_argument("--salt", default="", help="Salt value to use in hashing")
     pass
 
 def main():
